5e0501db72633dffe5e8700cc7f778141317123e
Gökalp Avcu <avcu219@gmail.com>
2022-11-16 09:50:39 +0300

    add changes

--- src/Components/Contacts/Form.js
+++ src/Components/Contacts/Form.js
@@ -1,10 +1,23 @@
 import { useState }  from 'react'
 
+import {useDispatch} from 'react-redux'
+import {addContact} from '../../redux/contactSlice'
+
 function Form() {
     const [name, setName] = useState('')
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault() // yazmamın sebebi sayfayı yenilemesini engellemek
+
+        console.log("hjdjhjjghdhgj");
+        dispatch(addContact({name}));
+
+    }
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}> 
             <input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
         </form>
     </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import contactSlice from './contactsSlice';
+import contactSlice from './contactSlice';
 
 export const store = configureStore({
     reducer: {
