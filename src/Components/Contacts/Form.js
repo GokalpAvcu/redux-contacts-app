bb7f703a4fb2849a8f395c5c039d9558dffeb8f9
Gökalp Avcu <avcu219@gmail.com>
2022-11-18 13:41:47 +0300

    added placeholder

--- src/Components/Contacts/Form.js
+++ src/Components/Contacts/Form.js
@@ -2,10 +2,11 @@ import { useState }  from 'react'
 
 import {nanoid} from '@reduxjs/toolkit';
 import {useDispatch} from 'react-redux'
-import {addContact, addContacts} from '../../redux/contactSlice'
+import {addContact} from '../../redux/contactSlice'
 
 function Form() {
     const [name, setName] = useState('')
+    const [number, setNumber] = useState('')
 
     const dispatch = useDispatch(); // dispatch kullanmamın sebebi redux ile bağlantı kurmak
 
@@ -13,10 +14,8 @@ function Form() {
         e.preventDefault() // yazmamın sebebi sayfayı yenilemesini engellemek
 
         if(!name ) return false;
-
-        const names = name.split(',');
-
-        dispatch(addContacts(names.map((name) => ({id: nanoid(), name}))));
+        
+        dispatch(addContact({ id: nanoid(), name}));
         setName('');
 
     };
@@ -24,6 +23,7 @@ function Form() {
     <div>
         <form onSubmit={handleSubmit}> 
             <input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <input placeholder="phone number" value={number} onChange={(e) => setNumber(e.target.value)} type="text" />
         </form>
     </div>
   )
